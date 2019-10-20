239c858486e517f83c3af00b0ba14ceeb7372943
Sean Nesbit <sfnesbit@calpoly.edu>
2019-10-20 15:18:03 -0700

    change .env json names

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,4 @@ import json
 def get_spotify_credentials():
     with open (".env") as json_file:
         data = json.load(json_file)
-        return (data["SPOTID"], data["SPOTSECRET"])
+        return (data["CLIENT_ID"], data["CLIENT_SECRET"])
