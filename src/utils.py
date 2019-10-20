489df17d6f7ddd2cc243d797afba64bfc3ad7926
Ryan Elliott <ryelliot@calpoly.edu>
2019-10-20 14:55:54 -0700

    Fix return placement

--- src/utils.py
+++ src/utils.py
@@ -4,5 +4,3 @@ def get_spotify_credentials():
     with open (".env") as json_file:
         data = json.load(json_file)
         return (data["SPOTID"], data["SPOTPASS"])
-
-print(get_spotify_credentials())
